71c7199551c9b0f7ba60c04b7826f9efec86ca72
Nick Karamoff <nikita.karamoff@gmail.com>
2017-04-21 21:34:56 +0300

    Implement Toolbar [BD-6]
    - add settings drawable for future use
    - implement toolbar with app name
    - change app name to "Bodio Alpha"
    - change version in gradle to "alpha-0.0.1"

--- app/src/main/java/ru/karamoff/bodio/MainActivity.java
+++ app/src/main/java/ru/karamoff/bodio/MainActivity.java
@@ -2,13 +2,23 @@ package ru.karamoff.bodio;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initializeToolbar();
+    }
+
+    private void initializeToolbar() {
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle(R.string.app_name);
     }
 }
 
